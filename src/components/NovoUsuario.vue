471c416899ac3924360d33fc49b70efac6f7b8b1
Marlos G Leite <marlosgleite@gmail.com>
2022-06-09 08:58:37 -0300

    WIP

---
+++ src/components/NovoUsuario.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: "NovoUsuario",
+  props: {
+    src: { type: String, required: true },
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<template>
+  <div></div>
+</template>
